71bf7ca1a6562fa3d967ceb67918fb0516506263
IgoryanDeltoro <ibondarc@c3r5p2.42vienna.com>
2025-06-25 11:45:08 +0200

    added struct

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -2,4 +2,18 @@
 # define CUB3D_H
 # include <stdio.h>
 
+typedef struct s_game
+{
+	void				*mlx;
+	void				*win;
+	char				**map;
+	int					map_width;
+	int					map_height;
+	int					def_size_img;
+	int					player_x;
+	int					player_y;
+	int					exit_x;
+	int					exit_y;
+}						t_game;
+
 #endif
--- src/cub3d.c
+++ src/cub3d.c
@@ -1,6 +1,13 @@
 #include "../includes/cub3d.h"
 
-int main()
+int main(int ac, char *av[])
 {
-    printf("Hello in cub3D game");
+    t_game game;
+
+    if (ac != 2)
+        return (printf("There are not right args!\n"), 1);
+    //check inputs map
+    //init game
+
+    return (0);
 }
